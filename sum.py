882eb43350e2a487b726365b90f11a92a55d9e01
Jeremy C. Andrus <jchristian.andrus@gmail.com>
2012-12-15 23:45:13 -0500

    improvements to summation python
    add the ability to directly parse the output of /proc/armperf
    sum all counter values and output both the number of samples and all the
    summed counts.
    
    Signed-off-by: Jeremy C. Andrus <jchristian.andrus@gmail.com>

--- sum.py
+++ sum.py
@@ -1,12 +1,21 @@
 #!/usr/bin/python
-import sys
+import sys, re
 
 if len(sys.argv) != 2:
-    sys.exit("usage: sum.py [file_with_numbers]")
+    sys.exit("usage: sum.py [/proc/armperf_output]")
 
-sum = 0
+nsamples = 0
+counter = re.compile(r'PMU.counter\[(\d+)\]= (\d+)')
+
+sum = [ 0, 0, 0, 0, 0, 0 ]
 for line in open(sys.argv[1]):
-    val = long(line)
-    sum += val
+    m = counter.match(line)
+    if m:
+        idx = int(m.group(1))
+        if idx == 0:
+            nsamples += 1
+        val = long(m.group(2))
+        sum[idx] += val
 
+print nsamples, " samples"
 print sum
